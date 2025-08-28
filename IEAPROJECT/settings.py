9a2d57347e37ecfba2a8d4a3be6181d5036758b4
akashmadha <akshmadha321@gmail.com>
2025-08-28 16:25:30 +0530

    changes

--- IEAPROJECT/settings.py
+++ IEAPROJECT/settings.py
@@ -124,11 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]        # your repo static folder
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+APPEND_SLASH = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
